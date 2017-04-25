213d61fcb8ac943dd25c5c683f451697604db8cd
Axtux <axtux@hotmail.com>
2017-04-25 13:27:36 +0200

    only compile instructions

--- src/yacc.py
+++ src/yacc.py
@@ -2,20 +2,24 @@ from ply import yacc
 # import lex.py lexemes
 from lex import tokens
 
-variables = {}
-
 """
     Grammar definition
 """
 
-def p_program_textcode_program(p) :
-  '''program : TEXT program
-             | code program'''
+def p_program_subprogram_program(p) :
+  '''program : subprogram program'''
   p[0] = p[1] + p[2]
-  
-def p_program_textcode(p) :
-  '''program : TEXT
-             | code'''
+
+def p_program_subprogram(p) :
+  '''program : subprogram'''
+  p[0] = p[1]
+
+def p_subprogram_text(p) :
+  '''subprogram : TEXT'''
+  p[0] = [('print', ('string', p[1]))]
+
+def p_subprogram_code(p) :
+  '''subprogram : code'''
   p[0] = p[1]
 
 
@@ -24,12 +28,12 @@ def p_code_codeblock(p) :
   p[0] = p[2]
 
 def p_codeblock_codeblock_codeline(p) :
-  '''codeblock : codeblock codeline'''
-  p[0] = p[1] + p[2]
+  '''codeblock : codeline codeblock'''
+  p[0] = [p[1]] + p[2]
 
 def p_codeblock_codeline(p) :
   '''codeblock : codeline'''
-  p[0] = p[1]
+  p[0] = [p[1]]
 
 def p_codeline_instruction(p) :
   '''codeline : instruction SEMICOLON'''
@@ -38,26 +42,21 @@ def p_codeline_instruction(p) :
 
 def p_instruction_print(p) :
   '''instruction : PRINT longstring'''
-  p[0] = p[2]
+  p[0] = ('print', p[2])
 
 def p_instruction_assign_string(p) :
   '''instruction : VARIABLE ASSIGNATION longstring
                  | VARIABLE ASSIGNATION stringlist'''
-  assign(p.lineno(1), p[1], p[3])
-  p[0] = ''
+  p[0] = ('assign', p[1], p[3])
 
 def p_instruction_for(p) :
   '''instruction : FOR VARIABLE IN enumarable DO codeblock ENDFOR'''
-  name = p[2]
-  p[0] = ''
-  for value in p[4] :
-    variables[name] = value
-    p[0] += p[6]
+  p[0] = ('for', p[2], p[4], p[6])
 
 
 def p_longstring_concat(p) :
   '''longstring : longstring CONCATENATION string'''
-  p[0] = p[1] + p[3]
+  p[0] = ('concat', p[1], p[3])
 
 def p_printable_string(p) :
   '''longstring : string'''
@@ -65,21 +64,11 @@ def p_printable_string(p) :
 
 def p_string_var(p) :
   '''string : VARIABLE'''
-  name = p[1]
-  if name in variables :
-    val = variables[name]
-    if type(val) == type('') :
-      p[0] = val
-    else :
-      yacc_error(p.lineno(1), 'variable {} is not string as expected'.format(name))
-      p[0] = ''
-  else :
-    yacc_error(p.lineno(1), 'variable {} undefined'.format(name))
-    p[0] = ''
+  p[0] = ('variable', p[1])
 
 def p_string_string(p) :
   '''string : STRING'''
-  p[0] = p[1]
+  p[0] = ('string', p[1])
 
 def p_enumarable_stringlist(p) :
   '''enumarable : stringlist'''
@@ -95,8 +84,7 @@ def p_stringnext_string(p) :
 
 def p_stringnext_stringnext(p) :
   '''stringnext : string COMA stringnext'''
-  p[0] = [p[1]]
-  p[0] = p[0] + p[3]
+  p[0] = [p[1]] + p[3]
 
 def p_error(p) :
   yacc_error(p.lineno, 'Syntax error')
@@ -111,23 +99,32 @@ def yacc_warning(lineno, message) :
 def yacc_error(lineno, message) :
   print('ERROR:', 'on line', lineno, ':', message)
 
-def assign(lineno, name, value) :
-  if name in variables :
-    yacc_warning(lineno, 'redefinition of variable {}'.format(name))
-  variables[name] = value
+"""
+    read arguments from command line
+"""
+
+debug = 0
+
+import sys
+
+for arg in sys.argv :
+  if arg == '-d' or arg == '--debug' :
+    debug = 1
 
 """
     Init parser
 """
-parser = yacc.yacc(debug=1)
+parser = yacc.yacc(debug=debug)
 
 """
     Launch parsing with system input if main script
 """
 
 if __name__ == "__main__" :
-  import sys
-  
   result = parser.parse(sys.stdin.read())
   
-  print('Output is {!r}'.format(result))
+  #print('Output is {!r}'.format(result))
+  print('Output is :\n')
+  
+  from pprint import pprint
+  pprint(result)
