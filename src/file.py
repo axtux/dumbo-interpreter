6e588eabfe0a1f8fe0bba25ba29f8c1a499a0c20
Axtux <axtux@hotmail.com>
2017-05-05 09:16:37 +0200

    add doc

--- src/file.py
+++ src/file.py
@@ -1,5 +1,20 @@
+"""Easy interface to manage text files."""
 
 def get_contents(filename) :
+  """Read whole file into a string.
+  
+  Parameters
+  ----------
+  filename : string
+    String filename absolute or relative to current working directory.
+  
+  Returns
+  -------
+  string
+    file contents on success
+  None
+    None if file is not readable
+  """
   try :
     f = open(filename, 'r')
   except OSError :
@@ -9,6 +24,10 @@ def get_contents(filename) :
   return contents
 
 def put_contents(filename, contents) :
+  """
+  write contents string into file
+  return True on success, False on error
+  """
   try :
     f = open(filename, 'w')
   except OSError :
