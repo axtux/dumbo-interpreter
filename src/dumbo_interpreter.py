29699af9f842d6fa1fd2f40feb45dc05dcb514ff
Axtux <axtux@hotmail.com>
2017-05-05 09:16:54 +0200

    use new file module

--- src/dumbo_interpreter.py
+++ src/dumbo_interpreter.py
@@ -1,24 +1,24 @@
 import sys
+import file
 
 from yacc import parser
 from execute import execute
-from files import *
 
 def main() :
   argc = len(sys.argv)
   if argc < 4 :
     return print('Usage: python3 {} data template output'.format(sys.argv[0]))
   
-  data = file_get_contents(sys.argv[1])
+  data = file.get_contents(sys.argv[1])
   if data == None :
     return print('data file "{}" not readable'.format(sys.argv[1]))
   
-  template = file_get_contents(sys.argv[2])
+  template = file.get_contents(sys.argv[2])
   if template == None :
     return print('template file "{}" not readable'.format(sys.argv[2]))
   
   output_name = sys.argv[3]
-  if file_get_contents(output_name) != None :
+  if file.get_contents(output_name) != None :
     return print('output file "{}" already exists'.format(output_name))
   
   data_code = parser.parse(data)
@@ -28,10 +28,10 @@ def main() :
   result = execute(data_code)
   result = execute(template_code)
   
-  if file_put_contents(output_name, result) :
-    print('Result saved successfully into {}'.format(output_name))
+  if file.put_contents(output_name, result) :
+    print('saved output to file "{}"'.format(output_name))
   else :
-    print('output file {} not writable'.format(output_name))
+    print('output file "{}" not writable'.format(output_name))
 
 if __name__ == "__main__" :
   main()
