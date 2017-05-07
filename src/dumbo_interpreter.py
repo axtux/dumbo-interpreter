5c80ea7ae1a6f9c90fef3061932241594b87c3ee
Axtux <axtux@hotmail.com>
2017-05-07 16:18:19 +0200

    use new parser interface

--- src/dumbo_interpreter.py
+++ src/dumbo_interpreter.py
@@ -1,7 +1,7 @@
 import sys
 import file
 
-from yacc import parser
+from yacc import parse_file
 from execute import execute
 
 def main() :
@@ -9,24 +9,27 @@ def main() :
   if argc < 4 :
     return print('Usage: python3 {} data template output'.format(sys.argv[0]))
   
-  data = file.get_contents(sys.argv[1])
-  if data == None :
-    return print('data file "{}" not readable'.format(sys.argv[1]))
-  
-  template = file.get_contents(sys.argv[2])
-  if template == None :
-    return print('template file "{}" not readable'.format(sys.argv[2]))
+  data_name = sys.argv[1]
+  template_name = sys.argv[2]
   
   output_name = sys.argv[3]
   if file.get_contents(output_name) != None :
     return print('output file "{}" already exists'.format(output_name))
   
-  data_code = parser.parse(data)
-  template_code = parser.parse(template)
+  data_code = parse_file(data_name)
+  if data_code == None :
+    return
+  template_code = parse_file(template_name)
+  if template_code == None :
+    return
   
-  # only save variables from data, not output
-  execute(data_code)
+  # save variables from data and template, output from template
+  result = execute(data_code)
+  if result == None :
+    return print('execution error into data file "{}"'.format(sys.argv[1]))
   result = execute(template_code)
+  if result == None :
+    return print('execution error into template file "{}"'.format(sys.argv[2]))
   
   if file.put_contents(output_name, result) :
     print('saved output to file "{}"'.format(output_name))
