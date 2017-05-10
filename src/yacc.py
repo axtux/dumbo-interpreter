811156c958620d675900a164352e950af3411389
Axtux <axtux@hotmail.com>
2017-05-10 14:39:04 +0200

    reorganise grammar, use lax.debug

--- src/yacc.py
+++ src/yacc.py
@@ -15,25 +15,21 @@ def p_program_subprogram(p) :
   '''program : subprogram'''
   p[0] = p[1]
 
+
 def p_subprogram_text(p) :
   '''subprogram : TEXT'''
   p[0] = [('print', infos(p), p[1])]
 
-def p_subprogram_code(p) :
-  '''subprogram : code'''
-  p[0] = p[1]
-
-
-
-def p_code_codeblock(p) :
-  '''code : CODESTART codeblock CODEEND'''
+def p_subprogram_codeblock(p) :
+  '''subprogram : CODESTART codeblock CODEEND'''
   p[0] = p[2]
 
-def p_code_empty(p) :
-  '''code : CODESTART CODEEND'''
+def p_subprogram_empty(p) :
+  '''subprogram : CODESTART CODEEND'''
   p[0] = []
 
-def p_codeblock_codeblock_codeline(p) :
+
+def p_codeblock_codeline_codeblock(p) :
   '''codeblock : codeline codeblock'''
   p[0] = [p[1]] + p[2]
 
@@ -45,6 +41,7 @@ def p_codeline_instruction(p) :
   '''codeline : instruction SEMICOLON'''
   p[0] = p[1]
 
+
 def p_instruction_print(p) :
   '''instruction : PRINT value'''
   p[0] = ('print', infos(p), p[2])
@@ -53,15 +50,9 @@ def p_instruction_assign(p) :
   '''instruction : VARIABLE ASSIGNATION value'''
   p[0] = ('assign', infos(p), p[1], p[3])
 
-def p_value_allop(p) :
-  '''value : boolop
-           | intop
-           | stringop
-           | stringlist'''
-  p[0] = p[1]
-
 def p_instruction_for(p) :
-  '''instruction : FOR VARIABLE IN enumarable DO codeblock ENDFOR'''
+  '''instruction : FOR VARIABLE IN variable DO codeblock ENDFOR
+                 | FOR VARIABLE IN stringlist DO codeblock ENDFOR'''
   p[0] = ('for', infos(p), p[2], p[4], p[6])
 
 def p_instruction_if(p) :
@@ -69,9 +60,24 @@ def p_instruction_if(p) :
   p[0] = ('if', infos(p), p[2], p[4])
 
 
-def p_boolop_boolop_bool(p) :
-  '''boolop : boolop AND bool
-            | boolop OR bool'''
+def p_value(p) :
+  '''value : variable
+           | boolop
+           | intop
+           | stringop
+           | stringlist'''
+  # value -> variable is already included in boolop, intop and stringop
+  # but this makes things clear
+  p[0] = p[1]
+
+def p_variable(p) :
+  '''variable : VARIABLE'''
+  p[0] = ('variable', infos(p), p[1])
+
+
+def p_boolop_bool_boolop(p) :
+  '''boolop : bool AND boolop
+            | bool OR boolop'''
   p[0] = (p[2], infos(p), p[1], p[3])
 
 def p_boolop_bool(p) :
@@ -82,16 +88,16 @@ def p_bool_boolean(p) :
   '''bool : BOOLEAN'''
   p[0] = p[1]
 
-def p_bool_comparison(p) :
-  '''bool : comparison'''
+def p_bool_variable(p) :
+  '''bool : variable'''
   p[0] = p[1]
 
-def p_comparison(p) :
-  '''comparison : intop COMPARATOR intop'''
+def p_bool_comparison(p) :
+  '''bool : intop COMPARATOR intop'''
   p[0] = (p[2], infos(p), p[1], p[3])
 
 
-def p_stringop_concat(p) :
+def p_stringop_string_stringop(p) :
   '''stringop : string CONCATENATION stringop'''
   p[0] = (p[2], infos(p), p[1], p[3])
 
@@ -99,34 +105,31 @@ def p_stringop_string(p) :
   '''stringop : string'''
   p[0] = p[1]
 
-
-def p_string_var(p) :
-  '''string : variable'''
-  p[0] = p[1]
-
 def p_string_string(p) :
   '''string : STRING'''
   p[0] = p[1]
 
-def p_enumarable_stringlist(p) :
-  '''enumarable : stringlist'''
+def p_string_variable(p) :
+  '''string : variable'''
   p[0] = p[1]
 
-def p_enumarable_variable(p) :
-  '''enumarable : variable'''
-  p[0] = p[1]
 
-def p_stringlist(p) :
-  '''stringlist : LEFT_PARENTHESE stringnext RIGHT_PARENTHESE'''
+def p_stringlist_stringseq(p) :
+  '''stringlist : LEFT_PARENTHESE stringseq RIGHT_PARENTHESE'''
   p[0] = p[2]
 
-def p_stringnext_string(p) :
-  '''stringnext : string'''
+def p_stringlist_empty(p) :
+  '''stringlist : LEFT_PARENTHESE RIGHT_PARENTHESE'''
+  p[0] = []
+
+def p_stringseq_string_stringseq(p) :
+  '''stringseq : string COMA stringseq'''
+  p[0] = [p[1]] + p[3]
+
+def p_stringseq_string(p) :
+  '''stringseq : string'''
   p[0] = [p[1]]
 
-def p_stringnext_stringnext(p) :
-  '''stringnext : string COMA stringnext'''
-  p[0] = [p[1]] + p[3]
 
 def p_intop_plus_minus(p):
   '''intop : intop PLUS term
@@ -146,7 +149,7 @@ def p_term_factor(p):
   '''term : factor'''
   p[0] = p[1]
 
-def p_factor_num(p):
+def p_factor_integer(p):
   '''factor : INTEGER'''
   p[0] = p[1]
 
@@ -154,32 +157,23 @@ def p_factor_variable(p):
   '''factor : variable'''
   p[0] = p[1]
 
-def p_variable(p) :
-  '''variable : VARIABLE'''
-  p[0] = ('variable', infos(p), p[1])
 
 def p_error(p) :
   print('ERROR syntax', infos(p))
 
-
-def parse_file(file_name) :
-  lexer = lex.parse_file(file_name)
-  return parser.parse(lexer=lexer)
-
 def infos(p) :
   return lex.token_info(p.lexer)
 
+
+def parse_file(file_name) :
+  lexer = lex.parse_file(file_name)
+  if lexer == None : return
+  return parser.parse(lexer=lexer)
+
 def init() :
-  'read command line arguments and init parser'
-  debug = 0
-  
-  for arg in sys.argv :
-    if arg == '-d' or arg == '--debug' :
-      debug = 1
-  
-  # init parser with defined grammar in this file
+  'init parser with defined grammar in this file'
   global parser
-  parser = ply.yacc.yacc(debug=debug)
+  parser = ply.yacc.yacc(debug=lex.debug)
 
 def main() :
   argc = len(sys.argv)
